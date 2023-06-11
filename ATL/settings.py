bc9485849ce8876b21f2cbf2071837b73d252d82
Bas-soft <bloom.softengine.com>
2023-06-11 14:43:19 +0100

    retry postgres

--- ATL/settings.py
+++ ATL/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'ATL.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-    }
+    'default': dj_database_url.config(conn_max_age=600,ssl_require=True)
 }
-
 
 
 # Password validation
